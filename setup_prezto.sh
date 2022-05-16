ab049e044fb231b49d8552e4a966c67638bec2cb
Eugen Blattner <eugen.blattner@mayflower.de>
2022-05-16 17:40:49 +0200

    fix: add missing line to remove an already existing .zshrc

--- setup_prezto.sh
+++ setup_prezto.sh
@@ -1,5 +1,6 @@
 #!zsh
 setopt EXTENDED_GLOB
+rm "${ZDOTDIR:-$HOME}/.zshrc"
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
