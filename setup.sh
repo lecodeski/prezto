20201374bac6ec79024882f7386198312d45f783
Eugen Blattner <eugen.blattner@mayflower.de>
2022-05-16 17:13:07 +0200

    fix: add missing eval for brew in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 "${ZDOTDIR:-$HOME}"/.zprezto/setup_brew.sh
+[[ $(uname) == "Linux" ]] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 $(brew --prefix)/bin/zsh "${ZDOTDIR:-$HOME}"/.zprezto/setup_prezto.sh
