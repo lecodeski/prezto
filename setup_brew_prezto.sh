ef2c08fa75902acf031ff9b59c96618ffeb2d0ed
Big Lecodeski <22894442+lecodeski@users.noreply.github.com>
2026-04-28 00:12:45 +0200

    add: install command for vim in setup_brew_prezto.sh install script

--- setup_brew_prezto.sh
+++ setup_brew_prezto.sh
@@ -12,6 +12,7 @@ brew install git
 # remove automatically installed but inferior completion script
 rm -f $(brew --prefix)/share/zsh/site-functions/_git
 
+brew install vim
 brew install eza
 brew install ripgrep
 brew install bat
