b77c4189272afcfa94f899e7c7cd6c052a0df235
Big Lecodeski <22894442+lecodeski@users.noreply.github.com>
2026-05-23 15:05:27 +0200

    fix: improve newline char ('\n') handling in fzf-history-widget
    when using the copy action

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -71,7 +71,7 @@ if [ $commands[fzf] ]; then
   export FZF_CTRL_R_OPTS="
     --preview 'echo {}' --preview-window up:3:hidden:wrap-word
     --bind 'ctrl-/:toggle-preview'
-    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --bind 'ctrl-y:execute-silent(printf %s {2..} | pbcopy)+abort'
     --color header:italic
     --header 'Press CTRL-Y to copy command into clipboard'"
 
